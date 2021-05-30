25f8561ab7086e7b29c0ce340b1dde57bcab80b6
cavalcant3 <jmatheusoc269@gmail.com>
2021-05-30 06:19:59 -0300

    customizando o header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ImgLogo from "./images/logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="header">
+        <img src={ImgLogo} className="header__img" />
+        <div className="header__button">
+          <button className="header__button_one">HOW WE WORK</button>
+          <button className="header__button_two">BLOG</button>
+          <button className="header__button_tree">ACCOUNT</button>
+          <button className="header__button_four"> VIEW PLANS</button>
+        </div>
+      </div>
+      <div className="card">
+        <h1 className="card__title">
+          Humanizing <br />
+          your insicurance.
+        </h1>
+        <p className="card__text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed sunt
+          facilis odio inventore repellat ratione ipsum obcaecati voluptatem
+          quis aspernatur ex tenetur, impedit unde deserunt perspiciatis qui
+          itaque eligendi enim.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button className="card__button">VIEW PLANS</button>
+      </div>
     </div>
   );
 }
