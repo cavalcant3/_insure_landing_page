5ca1ee966c9d6be0349587027df6060b1aaca602
cavalcant3 <jmatheusoc269@gmail.com>
2021-06-07 15:36:35 -0300

    adicionando propriedades

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
-import ImgLogo from "./images/logo.svg";
-import ImgBody from "./images/image-intro-desktop.jpg";
+
+const mainMenu = document.querySelector(".mainMenu");
+const openMenu = document.querySelector(".openMenu");
+const closeMenu = document.querySelector(".closeMenu");
+openMenu.addEventListener("click", show);
 
 function App() {
-  return <div className="App"></div>;
+  function show() {
+    mainMenu.style.display = "flex";
+    closeMenu.style.display = "block";
+  }
+  closeMenu.addEventListener("click", close);
+  function close() {
+    mainMenu.style.display = "none";
+    closeMenu.style.display = "none";
+  }
 }
 
 export default App;
