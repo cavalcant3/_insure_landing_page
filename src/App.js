747627156ce0317481eb80e40c4394edc6dec0d1
cavalcant3 <jmatheusoc269@gmail.com>
2021-06-14 15:14:30 -0300

    alterações

--- src/App.js
+++ src/App.js
@@ -227,6 +227,17 @@ function App() {
           </ul>
         </div>
       </section>
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        Coded by{" "}
+        <a href="https://github.com/cavalcant3" target="_blank">
+          Matheus Cavalcante
+        </a>
+        .
+      </div>
     </div>
   );
 }
